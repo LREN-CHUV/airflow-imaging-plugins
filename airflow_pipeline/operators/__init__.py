8c068fa52761fe237436b8bd55807d7ed1eca928
Ludovic Claude <ludovic.claude54@gmail.com>
2017-02-27 15:19:07 +0100

    Register new operators

--- airflow_pipeline/operators/__init__.py
+++ airflow_pipeline/operators/__init__.py
@@ -1,4 +1,6 @@
 from .prepare_pipeline_operator import PreparePipelineOperator
+from .bash_pipeline_operator import BashPipelineOperator
+from .docker_pipeline_operator import DockerPipelineOperator
 from .python_pipeline_operator import PythonPipelineOperator
 
-__all__ = ['PreparePipelineOperator', 'PythonPipelineOperator']
+__all__ = ['PreparePipelineOperator', 'BashPipelineOperator', 'DockerPipelineOperator', 'PythonPipelineOperator']
