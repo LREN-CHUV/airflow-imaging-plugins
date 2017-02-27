41fa3ca99881688d146e6b81f43fb09f7b340836
Ludovic Claude <ludovic.claude54@gmail.com>
2017-02-27 21:10:46 +0100

    Python is not self aware

--- airflow_spm/operators/spm_pipeline_operator.py
+++ airflow_spm/operators/spm_pipeline_operator.py
@@ -253,7 +253,7 @@ class SpmPipelineOperator(PythonOperator, TransferPipelineXComs):
                                                   others=json.dumps(provenance_details))
 
                 provenance_step_id = visit(self.task_id, output_folder, provenance_id,
-                                           previous_step_id=previous_step_id(),
+                                           previous_step_id=self.previous_step_id(),
                                            boost=self.boost_provenance_scan, session_id_by_patient=self.session_id_by_patient)
                 self.pipeline_xcoms['provenance_previous_step_id'] = provenance_step_id
 
