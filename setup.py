fe304e6f4da96fea99f2e0fd48acf8f024f593f3
Ludovic Claude <ludovic.claude54@gmail.com>
2017-03-30 17:32:52 +0200

    Bump version: 2.1.4 → 2.1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 from setuptools import setup, find_packages
 
-_version = '2.1.4'
+_version = '2.1.5'
 _packages = find_packages(exclude=["docs", "*.tests", "*.tests.*", "tests.*", "tests"])
 
 _short_description = ("pylint-common is a Pylint plugin to improve Pylint "
