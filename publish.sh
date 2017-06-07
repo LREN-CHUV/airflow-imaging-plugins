739c052d6c196e3023348ee19798e6c2b4b9ca6b
Ludovic Claude <ludovic.claude54@gmail.com>
2017-06-07 11:39:45 +0200

    Improve version check in publish script

--- publish.sh
+++ publish.sh
@@ -50,8 +50,7 @@ select_part() {
 git pull --tags
 # Look for a version tag in Git. If not found, ask the user to provide one
 [ $(git tag --points-at HEAD | wc -l) == 1 ] || (
-  latest_version=$(git describe --abbrev=00 || \
-    (bumpversion --dry-run --list patch | grep current_version | sed -r s,"^.*=",,) || echo '0.0.1')
+  latest_version=$( (bumpversion --dry-run --list patch | grep current_version | sed -r s,"^.*=",,) || echo '0.0.1')
   echo
   echo "Current commit has not been tagged with a version. Latest known version is $latest_version."
   echo
